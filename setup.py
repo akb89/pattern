dbed5beafefa568609682496e49b8054cbbd8920
akb89 <akb89@users.noreply.github.com>
2017-06-28 14:11:01 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ if sys.argv[-1] == "zip":
 
 setup(
         name="pattern",
-        version="3.0",
+        version="3.0.0beta",
         description="Web mining module for Python.",
         license="BSD",
         author="Tom De Smedt",
